665669e2a9a360cd5244ba0631d4fdbe34439eb0
Hadir Khan <hadirkhan10@gmail.com>
2023-10-29 12:41:36 -0700

    modified the testbench to use the leaky neuron

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    tt_um_hadirkhan10_lif_neuron tt_um_hadirkhan10_lif_neuron (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
